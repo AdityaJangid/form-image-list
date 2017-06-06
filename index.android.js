fdad6ba62df0ffae7105ee29214dbb1f72d32e7f
AdityaJangid <eradityajangid@gmail.com>
2017-06-06 16:11:19 +0530

    Added Image slider (Gallery)

--- index.android.js
+++ index.android.js
@@ -4,14 +4,19 @@
  * https://github.com/facebook/react-native
  * @flow
  */}
-
 import React, { Component } from 'react';
+import { View, ScrollView, Text, StatusBar } from 'react-native';
+import Carousel from 'react-native-snap-carousel';
+import { sliderWidth, itemWidth } from 'form_image_list/styles/SliderEntry.style';
+import SliderEntry from 'form_image_list/components/SliderEntry';
+import styles from 'form_image_list/styles/index.style';
+import { ENTRIES1, ENTRIES2 } from 'form_image_list/static/entries';
+
+
 import {
   AppRegistry,
   StyleSheet,
-  Text,
 	Button,
-  View,
 } from 'react-native';
 
 import { Form,
@@ -22,7 +27,77 @@ import { Form,
 import ImageSlider from 'react-native-image-slider';
 
 
-export default class SliderText extends Component {
+
+export default class example extends Component {
+
+    getSlides (entries) {
+        if (!entries) {
+            return false;
+        }
+
+        return entries.map((entry, index) => {
+            return (
+                <SliderEntry
+		  key={`carousel-entry-${index}` }
+                  even={(index + 1) % 2 === 0}
+                  {...entry}
+                />
+            );
+        });
+    }
+
+    get example1 () {
+        return (
+            <Carousel
+              sliderWidth={sliderWidth}
+              itemWidth={itemWidth}
+              firstItem={1}
+              inactiveSlideScale={0.94}
+              inactiveSlideOpacity={0.6}
+              enableMomentum={false}
+              containerCustomStyle={styles.slider}
+              contentContainerCustomStyle={styles.sliderContainer}
+              showsHorizontalScrollIndicator={false}
+              snapOnAndroid={true}
+              removeClippedSubviews={false}
+            >
+                { this.getSlides(ENTRIES1) }
+            </Carousel>
+        );
+    }
+
+    
+
+    render () {
+        return (
+            <View style={styles.container}>
+                <StatusBar
+                  translucent={true}
+                  backgroundColor={'rgba(0, 0, 0, 0.3)'}
+                  barStyle={'light-content'}
+                />
+                <View style={styles.colorsContainer}>
+                    <View style={styles.color1} />
+                    <View style={styles.color2} />
+                </View>
+                <ScrollView
+                  style={styles.scrollview}
+                  indicatorStyle={'white'}
+                  scrollEventThrottle={200}
+                >
+                    <Text style={styles.title}>Example 1</Text>
+                    <Text style={styles.subtitle}>No momentum | Scale | Opacity</Text>
+                    { this.example1 }
+                </ScrollView>
+            </View>
+        );
+    }
+}
+
+
+
+
+class SliderText extends Component {
 render() {
     return (
 	   
@@ -49,19 +124,10 @@ render() {
     ]}/>
    </View> 
 
-
-
-
-
-
     );
 }
 	
 }
-
-
-
-
 
 
 class form_image_list extends Component {
@@ -87,24 +153,4 @@ class form_image_list extends Component {
 
 
 
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('form_image_list', () => SliderText);
+AppRegistry.registerComponent('form_image_list', () => example);
